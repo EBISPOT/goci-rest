ab175d5cbf50913c58b34c33c9fadac92d939c55
Cinzia Malangone <cinzia@ebi.ac.uk>
2018-10-03 10:00:35 +0100

    Python scripts

---
+++ goci-python-scripts/api-sample-scripts/properties.py
@@ -0,0 +1,16 @@
+base_url = "http://www.ebi.ac.uk/gwas/rest/api/"
+
+studies = "studies/"
+pmidSearch = "search/findByPubmedId?pubmedId="
+studyAssociations = "associations?projection=associationsByStudySummary"
+
+snps = "singleNucleotidePolymorphisms/"
+rsidSearch = "search/findByRsId?rsId="
+snpAssociations = "associations?projection=associationsBySnpSummary"
+
+efoTraits = "efoTraits/"
+uriSearch = "search/findByEfoUri?uri="
+efoAssociations = "associations?projection=associationsByEfoTraitSummary"
+ 
+
+
