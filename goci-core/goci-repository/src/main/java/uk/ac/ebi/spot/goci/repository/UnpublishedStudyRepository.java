200e9e16a17ff666f4e7138c04b963c655fff76d
sprintell <abayomi@sprintell.org>
2025-01-10 13:53:54 +0000

    remove all migration files from the goci core module

--- goci-core/goci-repository/src/main/java/uk/ac/ebi/spot/goci/repository/UnpublishedStudyRepository.java
+++ goci-core/goci-repository/src/main/java/uk/ac/ebi/spot/goci/repository/UnpublishedStudyRepository.java
@@ -1,6 +1,9 @@
 package uk.ac.ebi.spot.goci.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import uk.ac.ebi.spot.goci.model.UnpublishedStudy;
@@ -10,4 +13,26 @@ public interface UnpublishedStudyRepository extends JpaRepository<UnpublishedStu
 
     @RestResource(path = "query", rel = "accession")
     UnpublishedStudy findByAccession(String accession);
+
+    @Query("Select study.accession as studyId, study.cohort as cohort " +
+
+            "from UnpublishedStudy study"
+
+                //            " WHERE (pub.pubmedId = :pubmedId OR :pubmedId = '*') " +
+                //            " AND ((lower(firstAuthor.fullnameStandard) = lower(:author)) OR :author = '*') " +
+                //            " AND (curator.id = :curator OR :curator = 0) " +
+                //            " AND (efoTraits.id = :efoTraitId OR :efoTraitId = 0) " +
+                //            " AND (diseaseTrait.id = :diseaseTraitId OR :diseaseTraitId = 0) " +
+                //            " AND (status.id LIKE :status OR :status = 0) " +
+                //            " AND (study.accessionId = :accessionId OR :accessionId = '*') " +
+                //            " AND (study.id = :studyId OR :studyId = 0) " +
+                //
+                //            " AND (study.gxe = :gxe OR :gxe IS NULL) " +
+                //            " AND (study.gxg = :gxg OR :gxg IS NULL) " +
+                //            " AND (study.cnv = :cnv OR :cnv IS NULL) " +
+                //            " AND (gtech.genotypingTechnology = :genotypeTech OR :genotypeTech = '*') " +
+                //
+                //            " AND ((lower(notes.textNote) LIKE lower(CONCAT('%',:notesQuery,'%'))) OR :notesQuery = '*') "
+    )
+    Page<UnpublishedStudy> findByMultipleFilters(Pageable pageable);
 }
